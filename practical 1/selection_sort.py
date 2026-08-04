65b83c719a27ce66a9c11e7c6a35464abc40aaa1
yenumulakishore127446-bit <yenumula.kishore127446@marwadiuniversity.ac.in>
2026-08-04 12:37:05 +0530

    Update selection_sort.py

--- practical 1/selection_sort.py
+++ practical 1/selection_sort.py
@@ -1,18 +1,22 @@
-# Selection Sort Program
+# Selection Sort in Python
 
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# Input
 arr = [64, 25, 12, 22, 11]
 
-n = len(arr)
+print("Original Array:", arr)
 
-for i in range(n):
-    min_index = i
+selection_sort(arr)
 
-    for j in range(i + 1, n):
-        if arr[j] < arr[min_index]:
-            min_index = j
-
-    # Swap the minimum element with the first unsorted element
-    arr[i], arr[min_index] = arr[min_index], arr[i]
-
-print("Sorted array:")
-print(arr)
+print("Sorted Array:", arr)
